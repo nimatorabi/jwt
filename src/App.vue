ffb3de8f3b240e2021d4efa3981c45ba6e8b4ff3
nimatorabi <46302001+nimatorabi@users.noreply.github.com>
2020-05-17 10:04:48 +0430

    complete jwt with vuex and axios

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,22 @@
 </template>
 
 <script>
+// import axios from "axios";
+// export default {
+//   created: function() {
+//     axios.interceptors.response.use(undefined, function(err) {
+//       return new Promise(function(resolve) {
+//         if (err.status === 401 && err.config && !err.config.__isRetryRequest) {
+//           this.$store.dispatch("logout");
+//           resolve();
+//         }
+//         throw err;
+//       });
+//     });
+//   }
+// };
 export default {
-  components: {}
+  created: function() {}
 };
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -3,26 +3,47 @@ import VueRouter from 'vue-router'
 import dashboard from '../views/dashboard.vue'
 import Login from '../components/Login.vue'
 import Home from '../views/Home.vue'
+import store from '../store'
 Vue.use(VueRouter)
 
+// const ifNotAuthenticated = (to, from, next) => {
+//   if (!store.getters.isLoggedIn) {
+//     next('/login')
+//     return
+//   }
+//   next('/')
+// }
+// const ifAuthenticated = (to, from, next) => {
+//   if (store.getters.isLoggedIn) {
+//     next()
+//     return
+//   }
+//   next('/login')
+// }
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: Home
+    component: Home,
+    meta: {
+      requiresAuth: true
+    }
+    // beforeEnter: ifNotAuthenticated
   },
   {
     path: '/login',
     name: 'login',
     component: Login,
-    meta: {
-      requiresVisitor: true,
-    }
+    // beforeEnter: ifAuthenticated
   },
   {
     path: '/dashboard',
     name: 'dashboard',
     component: dashboard,
+    meta: {
+      requiresAuth: true
+    }
+    // beforeEnter: ifNotAuthenticated
   },
 
 ]
@@ -31,5 +52,17 @@ const router = new VueRouter({
   mode: 'history',
   routes,
 })
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (store.getters.isLoggedIn) {
+      next()
+      return
+    }
+    next('/login')
+  } else {
+    next()
+  }
+})
+
 
 export default router
